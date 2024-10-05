879693859275ddc5c6e7a0021259083cc156cfb0
burakvural <burak.vural@aerlingus.com>
2024-10-05 20:35:25 +0300

    jacoco coverage page has been added to github actions

--- src/main/java/com/pinsoft/burakTask/BurakTaskApplication.java
+++ src/main/java/com/pinsoft/burakTask/BurakTaskApplication.java
@@ -3,6 +3,11 @@ package com.pinsoft.burakTask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class for the application.
+ *
+ * @Author Burak Vural
+ */
 @SpringBootApplication
 public class BurakTaskApplication {
 
